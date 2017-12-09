69fe4eb3b1f61d099f0e8c9bfce611f4ec8b613d
Baskaran Viswanathan <vibaskaran@live.com>
2017-12-09 12:26:15 -0500

    Final file
    This script is to read the csv file and collect the image of the speaker from his talk url

--- Tedtalks_collecting_image_url.py
+++ Tedtalks_collecting_image_url.py
@@ -8,15 +8,17 @@ from selenium import webdriver
 
 from bs4 import BeautifulSoup as B
 
-csv_input = pd.read_csv('Data/Main.csv'
+csv_input = pd.read_csv('Data/Main1.csv',header=0,skip_blank_lines=True)
 
 image_url_list = []
 failed_url_list = []
 url = ''
 try:
-    with open('Data/Main.csv', mode='r') as csvfile:
+    with open('Data/Main1.csv', mode='r') as csvfile:
         
         reader = csv.DictReader(csvfile)
+        # reader = itertools.islice(csv.DictReader(csvfile), 1000)
+
         for row in reader:
             url = row['url']
             if (url):
@@ -40,4 +42,82 @@ except:
     pass
 
 csv_input['image_url'] = image_url_list
-csv_input.to_csv('Data/output.csv', index=False)
+csv_input.to_csv('Data/output1.csv', index=False)
+
+
+
+csv_input = pd.read_csv('Data/Main2.csv',header=0,skip_blank_lines=True)
+
+
+image_url_list = []
+failed_url_list = []
+url = ''
+try:
+    with open('Data/Main2.csv', mode='r') as csvfile:
+        
+        reader = csv.DictReader(csvfile)
+        # reader = itertools.islice(csv.DictReader(csvfile), 1000)
+
+        for row in reader:
+            url = row['url']
+            if (url):
+                wd = webdriver.Chrome()
+                wd.get(url)
+
+                # And grab the page HTML source
+                html_page = wd.page_source
+                wd.quit()
+
+                # Now you can use html_page as you like
+                soup = B(html_page, "lxml")
+
+                image_url = soup.find('link', {'itemprop': 'image'}).get('href')
+            
+            else:
+                image_url = 'https://vignette.wikia.nocookie.net/international-entertainment-project/images/9/94/SpongeBob_SquarePants_%28SpongeBob_SquarePants%29.png'
+                #failed_url_list.append(url)
+            image_url_list.append(image_url)
+except:
+    pass
+
+csv_input['image_url'] = image_url_list
+csv_input.to_csv('Data/output2.csv', index=False)
+
+
+
+csv_input = pd.read_csv('Data/Main3.csv',header=0,skip_blank_lines=True)
+
+
+image_url_list = []
+failed_url_list = []
+url = ''
+try:
+    with open('Data/Main3.csv', mode='r') as csvfile:
+        
+        reader = csv.DictReader(csvfile)
+        # reader = itertools.islice(csv.DictReader(csvfile), 1000)
+
+        for row in reader:
+            url = row['url']
+            if (url):
+                wd = webdriver.Chrome()
+                wd.get(url)
+
+                # And grab the page HTML source
+                html_page = wd.page_source
+                wd.quit()
+
+                # Now you can use html_page as you like
+                soup = B(html_page, "lxml")
+
+                image_url = soup.find('link', {'itemprop': 'image'}).get('href')
+            
+            else:
+                image_url = 'https://vignette.wikia.nocookie.net/international-entertainment-project/images/9/94/SpongeBob_SquarePants_%28SpongeBob_SquarePants%29.png'
+                #failed_url_list.append(url)
+            image_url_list.append(image_url)
+except:
+    pass
+
+csv_input['image_url'] = image_url_list
+csv_input.to_csv('Data/output3.csv', index=False)
